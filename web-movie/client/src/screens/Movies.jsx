29faec46637e38454c0cbf400554f6369a595df1
thong2085 <129254706+thong2085@users.noreply.github.com>
2024-04-02 11:45:55 +0700

    set-up front-end for MoviesPage

--- web-movie/client/src/screens/Movies.jsx
+++ web-movie/client/src/screens/Movies.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import Layout from "../layout/Layout";
+import Filters from "../components/Filters";
+import { Movies } from "../data/MovieData";
+import Movie from "../components/Movie";
+import { CgSpinner } from "react-icons/cg";
 
-const Movies = () => {
-  return <div>Movies</div>;
+const MoviesPage = () => {
+  const maxPage = 10;
+  const [page, setPage] = useState(maxPage);
+  const HandleLoadingMore = () => {
+    setPage(page + maxPage);
+  };
+  return (
+    <Layout>
+      <div className="min-height-screen container mx-auto px-2 my-6">
+        <Filters />
+        <p className="text-lg font-medium my-6">
+          Total <span className="font-bold text-subMain">{Movies?.length}</span>{" "}
+          Movies Found
+        </p>
+        <div className="grid sm:mt-10 mt-6 xl:grid-cols-4 2xl:grid-cols-5 lg:grid-cols3 sm:grid-cols-2 gap-6">
+          {Movies.slice(0, page)?.map((movie, index) => (
+            <Movie key={index} movie={movie} />
+          ))}
+        </div>
+        {/* Loading More */}
+        <div className="w-full flex-colo md:my-20 my-10">
+          <button
+            onClick={HandleLoadingMore}
+            className="flex-rows gap-3 text-white py-3 px-8 rounded font-semibold border-2 border-subMain"
+          >
+            Loading More <CgSpinner className="animate-spin" />
+          </button>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
-export default Movies;
+export default MoviesPage;
