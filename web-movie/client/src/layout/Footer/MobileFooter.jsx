38e80aec71966f5349609438da2d0005584ece14
thong2085 <129254706+thong2085@users.noreply.github.com>
2024-04-13 08:39:23 +0700

    design front end navbar item favorites

--- web-movie/client/src/layout/Footer/MobileFooter.jsx
+++ web-movie/client/src/layout/Footer/MobileFooter.jsx
@@ -1,11 +1,16 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { CgMenuBoxed } from "react-icons/cg";
 import { FiHeart, FiHome, FiUserCheck } from "react-icons/fi";
 import { NavLink } from "react-router-dom";
 import MenuDrawer from "../../components/Drawer/MenuDrawer";
 import { SidebarContext } from "../../context/DrawerContext";
+import { useSelector, useDispatch } from "react-redux";
+import { getFavoriteMoviesAction } from "../../redux/Actions/userActions";
 
 const MobileFooter = () => {
+  const dispatch = useDispatch();
+  const { likedMovies } = useSelector((state) => state.userGetFavoriteMovies);
+
   const { mobileDrawer, toggleDrawer } = useContext(SidebarContext);
   const active =
     "rounded bg-white transitions text-main flex gap-3 items-center p-4";
@@ -14,6 +19,9 @@ const MobileFooter = () => {
     "rounded font-medium hover:text-subMain text-sm transitions flex gap-3 items-center p-4";
   const Hover = ({ isActive }) =>
     isActive ? `${active} ${hover}` : `${inActive} ${hover}`;
+  useEffect(() => {
+    dispatch(getFavoriteMoviesAction());
+  }, [dispatch]);
   return (
     <>
       <div className="flex flex-col justify-between align-middle h-full bg-white rounded cursor-pointer overflow-y-scroll flex-grow w-full">
@@ -27,8 +35,8 @@ const MobileFooter = () => {
           </NavLink>
           <NavLink to="/favorites" className={Hover}>
             <div className="relative">
-              <div className="w-5 h-5 flex-colo rounde-full text-xs bg-subMain text-white absolute -top-5 -right-5">
-                3
+              <div className="w-5 h-5 flex-colo rounded-full text-xs bg-subMain text-white absolute -top-4 -right-4">
+                {likedMovies?.length}
               </div>
               <FiHeart className="w-5 h-5" />
             </div>
